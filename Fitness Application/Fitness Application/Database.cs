a002a3d8b5bd0422bbbfee34b6769cb03693d704
Mathias Schrooten <mathiasschrooten@gmail.com>
2015-04-10 12:37:35 +0200

    Aanpassingen

--- Fitness Application/Fitness Application/Database.cs
+++ Fitness Application/Fitness Application/Database.cs
@@ -11,17 +11,31 @@ namespace Fitness_Application
     {
     
 
-       public async static Task<Boolean> InsertNewRun(Run run)
+       public static void InsertNewRun(Run run)
        {
-           SQLiteConnection connection;
-           SQLiteCommand command;
-           connection = new SQLiteConnection("data source=FitnessApplication.sqlite");
-           command = new SQLiteCommand(connection);
+           SQLiteConnection connection = new SQLiteConnection("data source=FitnessApplication.sqlite"); ;
+           SQLiteCommand command= new SQLiteCommand(connection);
+           try
+           {
+           
+           command.CommandText = "INSERT INTO FitnessTable (Time, Distance) Values (@time, @distance)";
+           command.Parameters.AddWithValue("@time", run.time);
+           command.Parameters.AddWithValue("@distance", run.distance);
            connection.Open();
+           command.ExecuteNonQuery();
+           }
+           catch (Exception ex)
+           {
+               Console.WriteLine("Error inserting run.");
 
-           //command.CommandText = "INSERT INTO FitnessTable ("
+           }
+           finally
+           {
+               connection.Close();
+           }
+           
+           
        }
 
-       
     }
 }
--- Fitness Application/Fitness Application/RunningWindow.xaml.cs
+++ Fitness Application/Fitness Application/RunningWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Data.SQLite;
+using System.Text.RegularExpressions;
 namespace Fitness_Application
 {
     /// <summary>
@@ -25,6 +26,8 @@ namespace Fitness_Application
 
             InitializeComponent();
             hideControls();
+            hhmmssLabel.Content = "(hh:mm:ss)";
+            
 
         }
 
@@ -50,6 +53,7 @@ namespace Fitness_Application
             timeTextbox.Visibility = Visibility.Hidden;
             saveButton.Visibility = Visibility.Hidden;
             hhmmssLabel.Visibility = Visibility.Hidden;
+            errorLabel.Visibility = Visibility.Hidden;
 
         }
 
@@ -61,6 +65,7 @@ namespace Fitness_Application
             timeTextbox.Visibility = Visibility.Visible;
             saveButton.Visibility = Visibility.Visible;
             hhmmssLabel.Visibility = Visibility.Visible;
+            errorLabel.Visibility = Visibility.Visible;
 
         }
 
@@ -75,33 +80,96 @@ namespace Fitness_Application
 
         private  void Button_Click(object sender, RoutedEventArgs e)
         {
-            // //Data_connection dbobject = new Data_connection();
-            // SQLiteConnection sqlConnection;
-            // SQLiteCommand sqlCommand;
-            string createTableQuery = @"CREATE TABLE IF NOT EXISTS [RunningTable](
-                            [ID] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                            [Time] NVARCHAR(20) NULL, [Distance] REAL NULL)";
+//            // //Data_connection dbobject = new Data_connection();
+//            // SQLiteConnection sqlConnection;
+//            // SQLiteCommand sqlCommand;
+//            string createTableQuery = @"CREATE TABLE IF NOT EXISTS [RunningTable](
+//                            [ID] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+//                            [Time] NVARCHAR(20) NULL, [Distance] REAL NULL)";
 
-            SQLiteConnection.CreateFile("FitnessApplication.sqlite");
-            SQLiteConnection connection = new SQLiteConnection("data source=FitnessApplication.sqlite");
+//            //SQLiteConnection.CreateFile("FitnessApplication.sqlite");
+//            SQLiteConnection connection = new SQLiteConnection("data source=FitnessApplication.sqlite");
 
-            SQLiteCommand command = new SQLiteCommand(connection);
-            connection.Open();
-            command.CommandText = createTableQuery;
-            command.ExecuteNonQuery();
-            command.CommandText = "INSERT INTO RunningTable (Time, Distance) Values ('22:14', 5.22)";
-            command.ExecuteNonQuery();
-            command.CommandText = "Select * FROM RunningTable";
-            using (SQLiteDataReader reader = command.ExecuteReader())
-            {
-                while (reader.Read())
-                {
-                    Console.WriteLine(reader["Time"] + ", distance: " + reader["Distance"]);
-                }
-            }
-            connection.Close();
+//            SQLiteCommand command = new SQLiteCommand(connection);
+//            connection.Open();
+//            //command.CommandText = createTableQuery;
+//            //command.ExecuteNonQuery();
+//            //command.CommandText = "INSERT INTO RunningTable (Time, Distance) Values ('22:14', 5.22)";
+//            //command.ExecuteNonQuery();
+//            command.CommandText = "Select * FROM RunningTable";
+//            using (SQLiteDataReader reader = command.ExecuteReader())
+//            {
+//                while (reader.Read())
+//                {
+//                    Console.WriteLine(reader["Time"] + ", distance: " + reader["Distance"]);
+//                }
+//            }
+//            connection.Close();
 
         }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            errorLabel.Visibility = Visibility.Hidden;
+            Run run = new Run();
+            if (isValid())
+            {
+                run.time =  TransformTime(timeTextbox.Text);
+                run.distance = Convert.ToDouble(distanceTextbox.Text);
+                //TODO: IMPLEMENT THE DATE THAT THE RUN WAS COMPLETED WITH A CALENDAR
+            }
+        }
+        private Time TransformTime(string time)
+        {
+            Time t = new Time();
+            if (timeTextbox.Text.Length > 5)
+            {
+                t.hours = Convert.ToInt32(timeTextbox.Text.Substring(0, 2));
+                t.minutes = Convert.ToInt32(timeTextbox.Text.Substring(3, 5));
+                t.seconds = Convert.ToInt32(timeTextbox.Text.Substring(6));
+                return t;
+            }
+            else
+            {
+                t.minutes = Convert.ToInt32(timeTextbox.Text.Substring(0, 2));
+                t.seconds = Convert.ToInt32(timeTextbox.Text.Substring(3));
+                return t;
+            }
+        }
+        private Boolean isValid()
+        {
+            if (distanceTextbox.Text == ""){
+               errorLabel.Content = "Gelieve een afstand in te vullen!";
+                errorLabel.Visibility = Visibility.Visible;
+                return false;
+            }
+            if (distanceTextbox.Text.Contains(',') || distanceTextbox.Text.Contains('.'))
+            {
+                distanceTextbox.Text.Replace('.', ':');
+                distanceTextbox.Text.Replace(',', ':');
+            }
+            if (timeTextbox.Text.Length > 8){
+                errorLabel.Content = "Gelieve een geldige tijd in te vullen!";
+                 errorLabel.Visibility = Visibility.Visible;
+                return false;
+            }
+            if (!timeTextbox.Text.Contains(':')){
+                errorLabel.Content = "Gelieve de tijd te scheiden met een dubbele punt (:)";
+                errorLabel.Visibility = Visibility.Visible;
+                return false;
+            }
+            if (Regex.Matches(timeTextbox.Text, @"a-zA-Z").Count > 0)
+            {
+                errorLabel.Content = "Gelieve enkel cijfers en ':' in  te geven in de tijd.";
+                errorLabel.Visibility = Visibility.Visible;
+                return false;
+            }
+
+            return true;
+
+        }
+
+
 
 
 
